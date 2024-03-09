bd2e94cfe65505a909bcd2179b565a6f618ebe0c
Amitstm <amitkumarmahto76@gmail.com>
2024-03-09 16:23:08 +0530

    namste

--- App.js
+++ App.js
@@ -1,31 +1,10 @@
-/** 
-
-<div id = "parent">
-    <div id = "child">
-    <h1>I'm h1 tag</h1>
-    </div>
-</div>
-*
-ReactElement(object) => HTML(Browser understand)
-*/
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h1 tag"),
-  ]),
-  
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-);
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React"
-// );
-console.log(parent);
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+// creat element
+const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+console.log(heading);
+//JSX
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading)
